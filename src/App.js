7b8e802da47da0273fddf69129e64a3635cd2b52
flying23 <45000726+flying23@users.noreply.github.com>
2018-12-29 02:50:00 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,92 +1,138 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Counter11 from "./Counter11.js";
-import Counter12 from "./Counter12.js";
+import "materialize-css/dist/css/materialize.min.css"
+import 'materialize-css'
 
 
 class App extends Component {
   constructor(props) {
-    super(props);
-
-    this.state = {
-      number: 0,
-    };
+    super(props)
+    this.state={
+      savedNotes: [ 
+        {
+          id:1,
+          title: "a",
+          content: "apple"
+        },
+        {
+          id:2,
+          title: "b",
+          content: "bee"
+        },
+        {
+          id:3,
+          title: "c",
+          content: "carrot"
+        }
+      ]
+    }
   }
 
-  handleIncrease2 = () => {
-    this.setState({ number: this.state.number +2 })
-  }
-
-  handleDecrease2 = () => {
-    this.setState({ number: this.state.number -2 })
-  }
-
-  handleReset2 = () => {
-    this.setState({ number: this.state.number=0 })
-  }
-
-
-  handleIncrease1 = () => {
-    this.setState({ number: this.state.number +1 })
-  }
-
-  handleDecrease1 = () => {
-    this.setState({ number: this.state.number -1 })
-  }
-
-  handleReset1 = () => {
-    this.setState({ number: this.state.number=0 })
+  handleSubmit = (userInputTitle, userInputContent) => {
+    const savedNotes = this.state.savedNotes
+    this.setState({
+      savedNotes: [
+        ...savedNotes,
+        {
+          id: savedNotes.length+1,
+          title: userInputTitle,
+          content: userInputContent
+        }
+      ]
+    })
   }
 
   render() {
-
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>
-            Counter2    
-          </h1>
-          <Counter11 num={"값: " +  this.state.number}
-            add={this.handleIncrease2}
-            sub={this.handleDecrease2}
-            reset={this.handleReset2} 
-
-            />
-        
-        
-    
-          <h1>
-            Counter1    
-          </h1>
-          <Counter12 num={"값: " + this.state.number} 
-            add={this.handleIncrease1}
-            sub={this.handleDecrease1}
-            reset={this.handleReset1}  
-            />
-        
-
-          
-          <a
-            className="App-link1"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-          <a
-            className="App-link2"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >  
-          </a>
-        </a></header>
+      <div>
+        <Writing submit={this.handleSubmit} />
+        <div className="row">
+          {this.state.savedNotes.map((note) => (
+            <Note key={note.id} title={note.title} content={note.content} />
+            ))}
+        </div>
       </div>
-    );
+      )
+    }
   }
-}
+  
+  
 
-export default App;
+  class Writing extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        userInputTitle: "",
+        userInputContent: "",
+      }
+    }
+      
+
+    handleSubmit = (e) => {
+      console.log('submitted')
+      this.props.submit(this.state.userInputTitle, this.state.userInputContent)
+      this.setState({
+        userInputTitle:"", userInputContent:""})
+      console.log(e.target);
+      e.preventDefault();
+    }
+
+    handleTitleChange = event => {
+      this.setState({
+        userInputTitle: event.target.value
+      })
+      console.log("userInputTitle is " + event.target.value)
+    }
+    handleContentChange = event => {
+      this.setState({
+        userInputContent: event.target.value
+      })
+      console.log("userInputContent is " + event.target.value)
+    }
 
 
+    render() { 
+      return (
+        <div>
+          <form onSubmit={this.handleSubmit} style={{margin: 80}}>
+            <div className="input-field" >
+              <input 
+              type="text"
+              value={this.state.userInputTitle}
+              onChange={this.handleTitleChange}
+              placeholder="제목"
+              />
+              <textarea
+                type="text"
+                className="comment" cols="50" rows="5"
+                value={this.state.userInputContent}
+                onChange={this.handleContentChange}
+                placeholder="Take a note"
+              ></textarea>
+              </div>
+              <input style={{width: "100%"}} className="btn green" type="submit" value="저장하기"/>
+          </form>
+        </div>
+      )
+      }
+    }
+  
+
+  class Note extends Component {
+    render() {
+      const title = this.props.title;
+      const content = this.props.content;
+      
+      return (
+        <div className='col s12 m6 l3'>
+          <div className='card yellow lighten-4'>
+            <div className='card-content black-text'>{title}</div>
+            <div className='card-content black-text'>{content}</div>
+          </div>
+        </div>
+        
+        )
+      }
+    }
+
+  export default App
